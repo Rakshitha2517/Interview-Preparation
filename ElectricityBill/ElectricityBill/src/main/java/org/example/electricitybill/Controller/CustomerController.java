16a7eb88af3d346160267a61bc2d605da55c665e
Rakshitha S <rakshi69292@gmail.com>
2026-04-24 19:01:50 +0530

    Add comments to CustomerController methods
    Added comments to methods for clarity.

--- ElectricityBill/ElectricityBill/src/main/java/org/example/electricitybill/Controller/CustomerController.java
+++ ElectricityBill/ElectricityBill/src/main/java/org/example/electricitybill/Controller/CustomerController.java
@@ -13,22 +13,22 @@ public class CustomerController {
 
     @Autowired
     private CustomerService service;
-
+//display customer
     @GetMapping
     public List<Customer> getCustomer() {
         return service.getAllCustomer();
     }
-
+//add customer
     @PostMapping
     public Customer addCustomer(@RequestBody Customer c) {
         return service.saveCustomer(c);
     }
-
+//update customer details
     @PutMapping("/{id}")
     public Customer update(@PathVariable int id, @RequestBody Customer c) {
         return service.updateCustomer(id, c);
     }
-
+//delete customer
     @DeleteMapping("/{id}")
     public String delete(@PathVariable int id) {
         return service.deleteCustomer(id);
